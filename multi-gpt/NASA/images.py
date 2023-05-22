81b9541f41a3401bff0d6136a38f86bdb68b5b60
ibnaleem <134088573+ibnaleem@users.noreply.github.com>
2023-05-22 18:43:43 +0100

    Update images.py

--- multi-gpt/NASA/images.py
+++ multi-gpt/NASA/images.py
@@ -12,3 +12,9 @@ class APOD:
         if not api_key:
             raise ValueError("api_key is required. Please obtain one from https://api.nasa.gov/")
         self.api_key = api_key
+
+    def generate(self, date: str = None, start_date: str = None, end_date: str = None):
+        if start_date is None and end_date or end_date is None and start_date:
+            raise ValueError(
+                "You must provide both the start_date and end_date but you only provided one of them. Either leave "
+                "both blank, provide both dates, or provide a date value.")
