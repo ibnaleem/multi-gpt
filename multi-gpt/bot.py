f4fe33bf8f9bd1232da64557d94a281c8116b043
ibnaleem <134088573+ibnaleem@users.noreply.github.com>
2023-05-22 18:48:39 +0100

    created bot.event

--- multi-gpt/bot.py
+++ multi-gpt/bot.py
@@ -8,3 +8,24 @@ from typing import Optional, List
 from pytube import YouTube
 from langs import langs
 from NASA.images import *
+
+chat_log = []
+
+with open('config.json') as f:
+    config = json.load(f)
+
+# Access the value of TOKEN
+TOKEN = config['TOKEN']
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), owner_id=1046920598359638036)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(
+        name="/help", url='https://www.twitch.tv/gothamchess'))
+    print(f"{bot.user} is online with the ID {bot.user.id}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands!")
+    except Exception as e:
+        print(e)
